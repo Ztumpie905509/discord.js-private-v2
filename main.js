f1cc896d313a837bd48a59fe0dc9b7c17b7292ce
Ztumpie <stumpie905509@gmail.com>
2021-11-23 10:47:17 +0800

    removed ts import

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import { Client, Intents, MessageEmbed } from "discord.js";
-import { isConstructorDeclaration } from "typescript";
 function sendEmbed(guildID, logChannel, embed) {
     client.guilds.fetch(guildID).then((guild) => {
         guild.channels.cache.get(logChannel).send({ embeds: [embed] })
