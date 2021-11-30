319ef0b3718a226addfcda8e3afb232621d46802
Ztumpie <stumpie905509@gmail.com>
2021-11-30 21:04:18 +0800

    added a space before image only

--- main.js
+++ main.js
@@ -203,7 +203,7 @@ client.on("message", (message) => {
     channelID = "811986481089347655"
     if (message.channel.id !== channelID) return
     if (!message.attachments.size) {
-        message.channel.send(`Dear ${message.author.username}:\nThis is an image only channel, please send images only.\nThank you for your cooperation!\n\Mokyuu~`)
+        message.channel.send(`Dear <@${message.author.id}>:\n\nThis is an image only channel, please send images only.\nThank you for your cooperation!\n\Mokyuu~`)
         message.delete()
     }
 })
